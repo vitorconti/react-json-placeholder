60471074e83a9b3489c57b7423c7a01102d44dfa
Vinícius Oliveira <eu@viniciusoliveira.me>
2021-12-09 12:53:45 -0300

    :recycle: refactor(fill-users): Refatorando o método fillUsersProperties.

--- src/helpers/fillUsers.js
+++ src/helpers/fillUsers.js
@@ -5,12 +5,23 @@ export async function fillUsersProperties(arrayToFill = []){
   try {
     const arrayFilled = []
     const iconPhotos  =['pi-facebook','pi-github','pi-twitter','pi-map','pi-youtube','pi-apple','pi-discord']
-    for (let item of arrayToFill){
-      const {name} = await getUserById(item.userId)
-      item.userName = name
-      item.userPhoto = iconPhotos[await getRandomInt(0,iconPhotos.length)]
-      arrayFilled.push(item);
-    }
+    const userIds = [...new Set(arrayToFill.map(x => x.userId))];
+
+    await Promise.all(userIds.map(async userId => {
+      let user = await getUserById(userId)
+      arrayFilled.push(
+        ...await Promise.all(
+          arrayToFill.filter(x => x.userId == userId)
+                  .map(async x => ({
+                    ...x,
+                    userName: user.name,
+                    userPhoto: iconPhotos[await getRandomInt(0, iconPhotos.length)]
+              })
+            )
+          )
+        )
+      })
+    );
     return arrayFilled
   } catch (error) {
     throw error
