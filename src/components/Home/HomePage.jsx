f013c5a2f7703e4cc7b0d51e0f52b00d40919f9c
Vitor Conti <vitorcontizgit@gmail.com>
2021-12-13 17:12:13 -0300

    home

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,11 +1,26 @@
 export default function HomePage() {
   return (
-    <div className ='m-16 text-center space-x-2'>
-      <h1 className='font-bold text-3xl'>Bem vindo ao React JSONPlaceholder!</h1>
-      <p className='mt-3 text-xl'>Esta aplicação tem por objetivo demonstrar a utilização do react no consumo de uma api-rest</p>
-      <p className='mt-3 text-xl'>A api utilizada encontra-se no link: <a className='font-bold leading-snug hover:opacity-75' href='https://jsonplaceholder.typicode.com/'>JSONPlaceholder</a></p>
-      <p className='mt-3 text-xl'>Para dar inicio a utilização da aplicação, navegue pelos items do menu na parte superior da tela</p>
-
+    <div className="m-16 text-center space-x-2">
+      <h1 className="font-bold text-3xl">
+        Bem vindo ao React JSONPlaceholder!
+      </h1>
+      <p className="mt-3 text-xl">
+        Esta aplicação tem por objetivo demonstrar a utilização do react no
+        consumo de uma api-rest
+      </p>
+      <p className="mt-3 text-xl">
+        A api utilizada encontra-se no link:{' '}
+        <a
+          className="font-bold leading-snug hover:opacity-75"
+          href="https://jsonplaceholder.typicode.com/"
+        >
+          JSONPlaceholder
+        </a>
+      </p>
+      <p className="mt-3 text-xl">
+        Para dar inicio a utilização da aplicação, navegue pelos items do menu
+        na parte superior da tela
+      </p>
     </div>
-  )
-};
+  );
+}
